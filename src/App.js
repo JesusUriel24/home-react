9e5a7e08819c67a6e0185f7da328fb1275cd877a
JesusUriel24 <solonosabes@gmail.com>
2023-10-08 20:50:54 -0600

    para samuel

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'; // Importa tu archivo de estilos CSS si aún no lo has hecho
+//import backgroundImage from './ruta-a-tu-imagen.jpg'; // Reemplaza 'ruta-a-tu-imagen.jpg' con la ruta real de tu imagen
 
 function App() {
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
+
+  //const headerStyle = {
+    //backgroundImage: `url(${backgroundImage})`, // Establece la imagen de fondo
+  //};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className={`app-header ${menuVisible ? 'menu-open' : ''}`}>
+        <div className="border-top"></div>
+        <div className="center-rectangle">
+          <button className="menu-button" onClick={toggleMenu}>☰</button>
+        </div>
+        {menuVisible && (
+          <nav className="dropdown-menu">
+            <ul>
+              <li><a href="/">Inicio</a></li>
+              <li><a href="/acerca-de">Acerca de</a></li>
+              <li><a href="/contacto">Contacto</a></li>
+            </ul>
+          </nav>
+        )}
       </header>
+      <main>
+        {/* Resto de tu contenido */}
+      </main>
     </div>
   );
 }
